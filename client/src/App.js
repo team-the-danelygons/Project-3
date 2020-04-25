b2bcdb909c2ae792d1617c79bf33fd5efc288d8c
chelsealuttrall <chelsealuttrall@gmail.com>
2020-04-25 17:57:44 -0600

    the start but lots more to do

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+<<<<<<< Updated upstream
+=======
+
+import logo from "./assets/images/ssfulllogo.png";
+import headerimg from "./assets/images/headerSMl.png";
+import {
+  Col,
+  Row,
+  Container,
+  Navbar,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+>>>>>>> Stashed changes
 
 class App extends Component {
   render() {
     return (
+<<<<<<< Updated upstream
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +30,63 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+=======
+      <>
+        <Navbar bg="light navbar-light">
+          <Navbar.Brand href="/">
+            <img
+              src={logo}
+              width="150"
+              height="30"
+              className="d-inline-block align-top"
+              alt="SAVESTANCE"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <p>
+              {(() => {
+                switch ("this.state.login") {
+                  case true:
+                    return (
+                      <Navbar.Text>
+                        Signed in as: {"NamePlaceHolder"}
+                      </Navbar.Text>
+                    );
+                  default:
+                    return <Navbar.Text>Create Account | Sign In</Navbar.Text>;
+                }
+              })()}
+            </p>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Container>
+          <Row>
+            <Col>
+              <div>
+                <br></br>
+                <h6>Search Stores</h6>
+                <InputGroup size="sm" className="mb-3">
+                  <FormControl
+                    aria-label="Small"
+                    aria-describedby="inputGroup-sizing-sm"
+                  />
+                </InputGroup>
+              </div>
+            </Col>
+            <Col xs={8} style={{backgroundColor: "#D3D3D3"}}>
+              <img src={headerimg} width="100%" alt="decoration"></img>
+            </Col>
+            <Col>2 of 2</Col>
+          </Row>
+          <Row>
+            <Col>Footer</Col>
+            
+          </Row>
+        </Container>
+      </>
+>>>>>>> Stashed changes
     );
   }
 }
