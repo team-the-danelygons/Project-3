1aa00894e0a7ad05000a57db717db98b97df2c5e
chelsealuttrall <chelsealuttrall@gmail.com>
2020-04-25 21:42:21 -0600

    buttons are added

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import React, { Component } from "react";
 import "./App.css";
 import logo from "./assets/images/logo_side.png";
 import headerimg from "./assets/images/headerSMl.png";
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//import { coffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserCheck, faUserTimes, faHeadSideMask, faHeadSideCough, faUserFriends, faHandsWash, faTimesCircle, faThumbsDown, faThumbsUp, faFlag, faThumbtack, faShare, faSave, faSearch
+ } from '@fortawesome/free-solid-svg-icons'
 
 import {
   Col,
@@ -56,16 +57,18 @@ class App extends Component {
             <Col border="success" style={{ backgroundColor: "#D3D3D3" }}>
               <div>
                 <br></br>
-                <h6>Search Stores</h6>
-                <InputGroup size="sm" className="mb-3">
+                <h6 className="controlPanel">Search Stores</h6>
+                <InputGroup size="sm" className="mb-3"><Button className="toTheRight" variant="secondary">
                   <FormControl
                     aria-label="Small"
                     aria-describedby="inputGroup-sizing-sm"
-                  />
+                    
+                  /><FontAwesomeIcon icon={faSearch} /></Button>
+                <br/>
                 </InputGroup>
-                <br />
-                <h6>Popular Stores</h6>
-                <hr />
+                
+                <h6 className="controlPanel">Popular Stores</h6>
+                
                 <p>King Soopers</p>
                 <p>Walmart</p>
                 <p>Home Depot</p>
@@ -83,32 +86,32 @@ class App extends Component {
             <Col style={{ backgroundColor: "#D3D3D3" }}>
               <div>
                 <br></br>
-                <h6>Control Panel</h6>
+                <h6 className="controlPanel">Control Panel</h6>
                 <div className="controlPanel">
                   <Col >
                     <Row className="spacing">
-                      <Button className="toTheLeft" variant="success" >1</Button>
-                      <Button className="toTheRight" variant="danger">2</Button>
+                      <Button className="toTheLeft" variant="success" ><FontAwesomeIcon icon={faUserCheck} /></Button>
+                      <Button className="toTheRight" variant="danger"><FontAwesomeIcon icon={faUserTimes} /></Button>
                     </Row>
                     <Row className="spacing">
-                      <Button className="toTheLeft" variant="success">1</Button>
-                      <Button className="toTheRight"variant="danger">2</Button>
+                      <Button className="toTheLeft" variant="success"><FontAwesomeIcon icon={faHeadSideMask} /></Button>
+                      <Button className="toTheRight"variant="danger"><FontAwesomeIcon icon={faHeadSideCough} /></Button>
                     </Row>
                     <Row className="spacing">
-                      <Button className="toTheLeft" variant="success">1</Button>
-                      <Button className="toTheRight"variant="danger">2</Button>
+                      <Button className="toTheLeft" variant="success"><FontAwesomeIcon icon={faHandsWash} /></Button>
+                      <Button className="toTheRight"variant="danger"><FontAwesomeIcon icon={faTimesCircle} /></Button>
                     </Row>
                     <Row className="spacing">
-                      <Button className="toTheLeft" variant="success">1</Button>
-                      <Button className="toTheRight" variant="danger">2</Button>
+                      <Button className="toTheLeft" variant="success"><FontAwesomeIcon icon={faThumbsUp} /></Button>
+                      <Button className="toTheRight" variant="danger"><FontAwesomeIcon icon={faThumbsDown} /></Button>
                     </Row>
                     <Row className="spacing">
-                      <Button className="toTheLeft" variant="success">1</Button>
-                      <Button className="toTheRight" variant="danger">2</Button>
+                      <Button className="toTheLeft" variant="secondary"><FontAwesomeIcon icon={faThumbtack} /></Button>
+                      <Button className="toTheRight" variant="secondary"><FontAwesomeIcon icon={faFlag} /></Button>
                     </Row>
                     <Row className="spacing">
-                      <Button className="toTheLeft" variant="success">1</Button>
-                      <Button className="toTheRight" variant="danger">2</Button>
+                      <Button className="toTheLeft" variant="secondary"><FontAwesomeIcon icon={faSave} /></Button>
+                      <Button className="toTheRight" variant="secondary"><FontAwesomeIcon icon={faShare} /></Button>
                     </Row>
                   </Col>
                   <Col ></Col>
