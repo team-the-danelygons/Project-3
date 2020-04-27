fca2ee6afd57a0a6606f9890441a444a6a813f7f
Jordan Servos <jservos11@yahoo.com>
2020-04-27 10:18:33 -0600

    created home, business, contact routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar"
+import Home from "./components/pages/Home";
+import Business from "./components/pages/Business";
+import Contact from "./components/pages/Contact";
 
-class App extends Component {
-  render() {
-    return (
-  <NavBar />
-     
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/business" component={Business} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
