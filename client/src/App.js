aa4ac125d15231be5bbbfbae5a85c8ce59c45bac
Jordan Servos <jservos11@yahoo.com>
2020-04-27 09:53:29 -0600

    created navbar as seperate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import logo from "./assets/images/ssfulllogo.png"
+import NavBar from "./components/NavBar/NavBar"
 
 class App extends Component {
   render() {
     return (
-      <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="/">
-        <img src={logo} width="150" height="30" className="d-inline-block align-top" alt=""/>
-      </a>
-    </nav>
+  <NavBar />
      
     );
   }
---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./nav.css";
+import logo from "../../assets/images/ssfulllogo.png"
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        <img src={logo} width="150" height="30" className="d-inline-block align-top" alt=""/>
+      </a>
+    </nav>
+     
+    );
+  }
+}
+
+export default NavBar;
