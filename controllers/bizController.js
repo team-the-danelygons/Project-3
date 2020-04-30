667f40eb73de98bb17c19720914b436d719df8c7
Jordan Servos <jservos11@yahoo.com>
2020-04-29 21:17:44 -0600

    updated bizController

--- controllers/bizController.js
+++ controllers/bizController.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   update: function(req, res) {
     db.Business
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .findOneAndUpdate({ _id: req.params.id }, req.body, {new: true})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
