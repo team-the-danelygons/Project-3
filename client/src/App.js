27d26c6521493163a09b17e1637f9fda1bbdcbab
Sean Jacobson <sean.jacobson86@gmail.com>
2020-05-05 12:18:40 -0600

    Added to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authAcations";
 import "./App.css";
 
 import { Provider } from 'react-redux';
@@ -11,25 +14,47 @@ import Business from "./components/pages/Business/Business";
 import Contact from "./components/pages/Contact/Contact";
 import Signup from "./components/pages/Signup/Signup";
 import Login from "./components/pages/Login/Login";
+import PrivateRoute from "./components/private-route/PrivateRoutes"
 // This comment is so I can push up the changes.
 
-function App() {
-  return (
-    <Provider store={ store }>
-      <Router>
-        <div>
-          <NavBar />
-          <Switch>
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div>
+            <NavBar />
             <Route exact path="/" component={Home} />
             <Route exact path="/business/:id" component={Business} />
             <Route path="/contact" component={Contact} />
             <Route path="/signup" component={Signup} />
             <Route path="/login" component={Login} />
-          </Switch>
-        </div>
-      </Router>
-    </Provider>
-  );
+            <Switch>
+              <PrivateRoute exact path="/" component={Home} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
