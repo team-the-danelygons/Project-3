2eaa17927122b943f8689cc0e9ffa8b837c5c005
Sean Jacobson <sean.jacobson86@gmail.com>
2020-05-05 10:23:31 -0600

    Created files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+
+import { Provider } from 'react-redux';
+import store from "./store"
+
 import NavBar from "./components/NavBar/NavBar"
 import Home from "./components/pages/Home/Home";
 import Business from "./components/pages/Business/Business";
@@ -11,18 +15,20 @@ import Login from "./components/pages/Login/Login";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <NavBar />
-        <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/business/:id" component={Business}  />
-        <Route path="/contact" component={Contact} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={ store }>
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/business/:id" component={Business} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
