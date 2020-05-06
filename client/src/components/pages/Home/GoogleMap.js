d6cf913e5f816b0e96952775e07278ac22c02e10
Cash <56851337+mlerey22@users.noreply.github.com>
2020-05-06 17:15:02 -0500

    Got the map on home page styling to work!!

--- client/src/components/pages/Home/GoogleMap.js
+++ client/src/components/pages/Home/GoogleMap.js
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 import GoogleMapReact from 'google-map-react';
 
 const Wrapper = styled.main`
-  width: 60%;
-  height: 60%;
+  width: 100%;
+  height: 100%;
 `;
 
 const GoogleMap = ({ children, ...props }) => (
